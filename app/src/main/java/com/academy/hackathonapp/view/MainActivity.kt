37d1d1e9decda060b01aa287f9ac58ed153834a3
VladOleg <31069701+binkos@users.noreply.github.com>
2019-11-22 20:35:56 +0300

    Navigation Drawer and toolbar are added to main screen
    Need to fix black burger at the right side

--- app/src/main/java/com/academy/hackathonapp/view/MainActivity.kt
+++ app/src/main/java/com/academy/hackathonapp/view/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.academy.hackathonapp.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.academy.hackathonapp.R
 import com.academy.hackathonapp.data.model.Error
 import com.academy.hackathonapp.data.model.Users
 import com.academy.hackathonapp.mvvm.Status
 import com.academy.hackathonapp.mvvm.viewModel.ActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,14 +21,22 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.academy.hackathonapp.R.layout.activity_main)
 
         activityViewModel = ViewModelProviders.of(this).get(ActivityViewModel::class.java)
+
+
+        drawer.closeDrawer(GravityCompat.START)
+        setSupportActionBar(my_toolbar)
+
         observeGetPosts()
+
 
         buttonOneClickListener()
         buttonTwoClickListener()
     }
+
+
 
     private fun observeGetPosts() {
         activityViewModel.simpleLiveData.observe(this, Observer {
@@ -86,4 +97,17 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(applicationContext, error.errorMsg, Toast.LENGTH_SHORT).show()
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(com.academy.hackathonapp.R.menu.main_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (drawer.isDrawerOpen(GravityCompat.START))drawer.closeDrawer(GravityCompat.START)
+        else drawer.openDrawer(GravityCompat.START)
+        return super.onOptionsItemSelected(item)
+    }
+
 }
