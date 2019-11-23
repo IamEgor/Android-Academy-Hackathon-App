154cad524c9cced2e93b26c2b39307fd7128162e
VladOleg <31069701+binkos@users.noreply.github.com>
2019-11-23 03:52:09 +0300

    nav navClickSelected is added

--- app/src/main/java/com/academy/hackathonapp/view/MainActivity.kt
+++ app/src/main/java/com/academy/hackathonapp/view/MainActivity.kt
@@ -3,19 +3,16 @@ package com.academy.hackathonapp.view
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.academy.hackathonapp.data.model.Error
-import com.academy.hackathonapp.data.model.Users
-import com.academy.hackathonapp.mvvm.Status
+import com.academy.hackathonapp.R
 import com.academy.hackathonapp.mvvm.viewModel.ActivityViewModel
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var activityViewModel: ActivityViewModel
 
@@ -27,11 +24,11 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction().replace(linLay.id,MainFragment()).commit()
 
         drawer.closeDrawer(GravityCompat.START)
+        nav_view.setNavigationItemSelectedListener(this)
         setSupportActionBar(my_toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-
         menuInflater.inflate(com.academy.hackathonapp.R.menu.main_menu,menu)
         return super.onCreateOptionsMenu(menu)
     }
@@ -40,6 +37,15 @@ class MainActivity : AppCompatActivity() {
         if (drawer.isDrawerOpen(GravityCompat.START))drawer.closeDrawer(GravityCompat.START)
         else drawer.openDrawer(GravityCompat.START)
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.categories){
+            return true
+        }
+
+
+        return false
     }
 
 
